6ed5d3461ad90f5de83fdfe1bda8176cc3f7a505
mbaj <01178508@pw.edu.pl>
2025-01-02 18:19:14 +0100

    Add BusMarker component and BusPosition type definition

---
+++ frontend/src/components/busMarker.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BusPosition } from "@/types";
+
+interface BusMarkerProps {
+  position: BusPosition;
+  busNumber: number;
+}
+
+const busMarker = (props: BusMarkerProps) => {
+  return <div></div>;
+};
+
+export default busMarker;
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2,3 +2,8 @@ export type RegisterData = {
   email_address: string;
   password: string;
 };
+
+export type BusPosition = {
+  X: number;
+  Y: number;
+};
