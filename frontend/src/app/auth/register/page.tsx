2915399310599af28b83f71828dff93b6def9b16
Krzysztof Król <krzysztof.krol.stud@pw.edu.pl>
2025-01-02 15:37:13 +0100

    initial logging in commit

---
+++ frontend/src/app/auth/register/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import RegisterForm from "@/components/registerForm";
+
+const Register = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import LogoutButton from "@/components/logoutButton";
+
+export default function Home() {
+  return <div></div>;
+}
