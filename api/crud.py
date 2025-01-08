539a1977cc5a4171f74387438baf0f3c08fb3fbf
Adam Kraś <adam.kras.stud@pw.edu.pl>
2025-01-08 16:16:28 +0100

    Moved the function to file with other cruds

--- api/crud.py
+++ api/crud.py
@@ -1,6 +1,9 @@
 from sqlmodel import Session, select
 from app.models.notification import Notification
 from typing import List
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+from data_retrieval.config import settings
 
 
 def create_notification(session: Session, email: str, line: str, stop: str, time: str) -> Notification:
@@ -22,3 +25,19 @@ def delete_notification(session: Session, notification_id: int) -> None:
     if notification:
         session.delete(notification)
         session.commit()
+
+def check_bus_status(line: str) -> bool:
+    client = MongoClient(f"mongodb://{settings.MONGO_USERNAME}:{settings.MONGO_PASSWORD}@{settings.MONGO_HOST}")
+    db = client[settings.MONGO_DATABASE]
+    collection = db.get_collection()
+
+    current_time = datetime.now()
+
+    bus_data = collection.find({"Lines": line}).sort("Time", -1).limit(1)
+
+    if bus_data:
+        last_movement_time = datetime.strptime(bus_data[0]["Time"], "%Y-%m-%d %H:%M:%S")
+        if current_time - last_movement_time <= timedelta(minutes=2):
+            return True
+
+    return False
