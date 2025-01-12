1028a262814f4ab5513df500391f7f969fcf4482
Adam Filewicz <adam.filewicz.stud@pw.edu.pl>
2025-01-12 12:00:23 +0100

    rebasing main into login

--- api/app/api/routes/auth.py
+++ api/app/api/routes/auth.py
@@ -8,6 +8,11 @@ from app.core.security import (
     create_access_token,
 )
 from app.core.db import get_session
+from authlib.integrations.starlette_client import OAuth
+from app.core.config import settings
+from starlette.requests import Request
+from starlette.config import Config
+from starlette.responses import JSONResponse
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -16,7 +21,7 @@ def register(user_create: UserCreate, session: Session = Depends(get_session)):
     if get_user_by_email(session, user_create.email):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Username already registered",
+            detail="Email already registered",
         )
     hashed_password = get_password_hash(user_create.password)
     return create_user(session, user_create.email, hashed_password)
@@ -35,3 +40,31 @@ def login(
         )
     access_token = create_access_token(data={"id": user.id, "email": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
+
+oauth = OAuth()
+oauth.register(
+    name="google",
+    client_id=settings.GOOGLE_CLIENT_ID,
+    client_secret=settings.GOOGLE_CLIENT_SECRET,
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_kwargs={"scope": "openid email profile"},
+)
+
+@router.get("/login/google")
+async def login_via_google(request: Request):
+    redirect_uri = settings.GOOGLE_REDIRECT_URI
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+@router.get("/")
+async def google_redirect_handler(request: Request):
+    try:
+        token = await oauth.google.authorize_access_token(request)
+        user_info = token.get("userinfo")
+        if not user_info:
+            raise HTTPException(status_code=400, detail="Could not fetch user info")
+
+        access_token = create_access_token(data={"sub": user_info["email"]})
+        # access_token = create_access_token(data={"id": user.id, "email": user.email})
+        return JSONResponse(content={"access_token": access_token, "user": user_info})
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Authentication failed")
