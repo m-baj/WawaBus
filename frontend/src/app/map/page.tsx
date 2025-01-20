3539636856ac14bd4ec34c6f904492c9f393cab8
Paweł Skorupski <paw.sko130@gmail.com>
2025-01-20 23:27:20 +0100

    Env fixes

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -104,7 +104,6 @@ export default function MapPage() {
                 let selectedTime = e.target.value;
                 if (!selectedTime) {
                   const now = new Date();
-                  now.setHours(now.getHours() + 1);
                   selectedTime = now.toISOString().slice(0, 16);
                 }
                 const formattedIso = `${selectedTime}:00.000Z`;
